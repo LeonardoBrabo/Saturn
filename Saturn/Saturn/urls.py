20e6df7c04989327fd0b66614cebae39fdc853ee
mlferreyra <67713005+mlferreyra@users.noreply.github.com>
2020-09-16 01:25:45 -0300

    Login, Logout y register

--- Saturn/Saturn/urls.py
+++ Saturn/Saturn/urls.py
@@ -20,7 +20,7 @@ from users import views
 urlpatterns = [
     path('', views.welcome),
     path('register', views.register),
-    path('login', views.login),
+    path('login/', views.login),
     path('logout', views.logout),
 
     path('admin/', admin.site.urls),
